a22fc3276581fa0f8762c0096ebb3ca42c0cdfac
mahabharathir <mahabharathi.r@gmail.com>
2026-04-10 13:48:41 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,66 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        //board.length == 9
+        //board[i].length == 9
+        //board[i][j] is a digit 1-9 or '.'.
+
+       /* //Approach 1
+        //Each row must contain the digits 1-9 without duplicates.
+        //Each column must contain the digits 1-9 without duplicates.
+        for(int i=0;i<9;i++){
+            int[] rowSeen =new int[9];
+            int[] colSeen =new int[9];
+            for(int j=0;j<9;j++){
+                char rowChar = board[i][j];
+                char colChar = board[j][i];
+                int rowValue = rowChar-'0';
+                int colValue = colChar-'0';
+                if(rowChar != '.' ){
+                    if(rowSeen[rowValue-1]!=0){
+                        return false;
+                    }
+                    rowSeen[rowValue-1]=1;
+                }
+                
+                if(colChar!='.'){
+                    if(colSeen[colValue-1]!=0){
+                        return false;
+                    }
+                    colSeen[colValue-1]=1;
+                }
+            }
+        }
+        //Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                int[] boxSeen =new int[9];
+                for(int k=0;k<3;k++){
+                    for(int l=0;l<3;l++){
+                        char c = board[i+k][j+l];
+                        if(c!='.'){
+                            int index =c-'0';
+                            if(boxSeen[index-1]!=0){
+                                return false;
+                            }
+                            boxSeen[index-1]=1;
+                        }
+                    }
+                }
+            }
+        }*/
+        Set seen = new HashSet();
+        for(int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                char c=board[i][j];
+                if(c!='.'){
+                    if(!seen.add(c+"row"+i) || !seen.add(c+"col"+j) || 
+                        !seen.add(c+"board"+i/3+"."+j/3)){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
