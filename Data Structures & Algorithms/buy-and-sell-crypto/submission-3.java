bf85a78da65ae90e92e425fff814276a1758a52b
mahabharathir <mahabharathi.r@gmail.com>
2026-04-12 15:14:01 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        //Approach 1 -brute force
+        int profit =0;
+        /*for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                int diff = prices[j]-prices[i];
+                profit =Math.max(profit, diff);
+            }
+        }*/
+
+        //Approach 2 
+        /*int left =0;
+        for(int right=1;right<prices.length;right++){
+            if(prices[left]<prices[right]){
+                profit=Math.max(prices[right]-prices[left], profit);
+            }else{
+                left =right;
+            }
+        }*/
+
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }else{
+                int diff =prices[i]-minPrice;
+                profit =Math.max(profit, diff);
+            }
+        }
+        return profit;
+    }
+}
